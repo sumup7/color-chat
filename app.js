59c2fea3606183387bb383eb6b52ca5fd9931126
sumup7 <sasaya7@gmail.com>
2020-01-28 09:04:12 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000;
+const helmet = require('helmet');
+var fs   = require('fs');
+
+app.use(helmet());
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req,res) {
+ res.sendFile(__dirname +'/public/index.html')
+});
+
+io.on('connection', function(socket){
+    var name= '';
+    socket.on('client_to_server_join', function (data) {
+      name = data.name;
+      io.emit('server_to_client_join', { join: name + "さんが入室しました。" });
+  });
+    socket.on('chat message', function(data){
+      io.emit('chat message', {name:data.name,message:data.message});
+    });
+    socket.on('disconnect',function(){
+      io.emit('server_to_client_leaving',{leaving: name + 'さんが退室しました。'});
+    });
+  });
+
+
+
+http.listen(PORT,function() {
+    console.log('listening on' + PORT);
+});
